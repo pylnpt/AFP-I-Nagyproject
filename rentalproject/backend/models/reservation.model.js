f56d1e0b4b869ac02dc54b81b95ae2b2da2c9640
Széles-Kovács Gyula <szeleskovacsgyula@yahoo.com>
2020-11-30 09:24:33 +0100

    update reservation model

---
+++ rentalproject/backend/models/reservation.model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const reservationSchema = new Schema({
+    firstName: {type: String, trim: true},
+    lastName: {type: String, trim: true},
+    email: {type: String, trim: true},
+    phoneNumber: {type: String, trim: true},
+    plateNumber: {type: String, trim: true}
+}, {
+    timestamps: true,
+});
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+module.exports = Reservation;
