a96cf4814aeedab87c837af2d0b5e4db69b6a4bf
Péter Molnár <xodax9898@gmail.com>
2020-12-01 08:47:23 +0100

    a reservations.js fájlban konstansok megadása

---
+++ rentalproject/backend/routes/reservations.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+let Reservation = require('../models/reservation.model');
+
+router.route('/').get((req, res) =>{
+    Reservation.find()
+        .then(reservations => res.json(reservations))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res) => {
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
+    const phoneNumber = req.body.phoneNumber;
+    const plateNumber = req.body.plateNumber;
+
+    const newReservation = new Reservation({
+        firstName,
+        lastName,
+        email,
+        phoneNumber,
+        plateNumber
+    });
+
+    newReservation.save()
+    .then(() => res.json('Reservation added!'))
+    .catch(err => res.status(400).json('Error ' + err));
+});
