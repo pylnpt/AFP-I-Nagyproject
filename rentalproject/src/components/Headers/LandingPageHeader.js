d004da083f846f1a7a013a129241798b785afb16
Levi0317 <65221472+Levi0317@users.noreply.github.com>
2020-12-14 20:54:02 +0100

    LandingPageHeader.js tartalmának feltöltése

---
+++ rentalproject/src/components/Headers/LandingPageHeader.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+// reactstrap components
+import { Button, Container } from "reactstrap";
+
+// core components
+
+function LandingPageHeader() {
+  let pageHeader = React.createRef();
+
+  React.useEffect(() => {
+    if (window.innerWidth < 991) {
+      const updateScroll = () => {
+        let windowScrollTop = window.pageYOffset / 3;
+        pageHeader.current.style.transform =
+          "translate3d(0," + windowScrollTop + "px, 0)";
+      };
+      window.addEventListener("scroll", updateScroll);
+      return function cleanup() {
+        window.removeEventListener("scroll", updateScroll);
+      };
+    }
+  });
+
+  return (
+    <>
+      <div
+        style={{
+          backgroundImage: "url(" + require("assets/img/car_road.jpg") + ")"
+        }}
+        className="page-header"
+        data-parallax={true}
+        ref={pageHeader}
+      >
+        <div className="filter" />
+        <Container>
+          <div className="motto text-center">
+            <h1>Stacionárius autókölcsönző</h1>
+            <br />
+            <Button
+              href="https://www.youtube.com/watch?v=prQ3QkTsvLw"
+              className="btn-round mr-1"
+              color="neutral"
+              target="_blank"
+              outline
+            >
+              <i className="fa fa-play" />
+              Hogyan bérelhetsz autót nálunk?
+            </Button>
+          </div>
+        </Container>
+      </div>
+    </>
+  );
+}
+
+export default LandingPageHeader;
