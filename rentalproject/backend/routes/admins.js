d726fefc2ae89bbefd3d1ac16219700538e5998e
Széles-Kovács Gyula <szeleskovacsgyula@yahoo.com>
2020-12-15 09:38:44 +0100

    formatting backend files

--- rentalproject/backend/routes/admins.js
+++ rentalproject/backend/routes/admins.js
@@ -3,76 +3,74 @@ const router = express.Router();
 let Admin = require('../models/admin.model');
 const jwt = require('jsonwebtoken');
 
-router.get('/', (req, res) =>{
-    Admin.find()
-        .then(admins => res.json(admins))
-        .catch(err => res.status(400).json('Error: ' + err));
+router.get('/', (req, res) => {
+  Admin.find()
+    .then(admins => res.json(admins))
+    .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.get('/:id', (req, res) => {
-    Admin.findById(req.params.id)
-        .then(admin => res.json(admin))
-        .catch(err => res.status(400).json('Error' + err));
+  Admin.findById(req.params.id)
+    .then(admin => res.json(admin))
+    .catch(err => res.status(400).json('Error' + err));
 });
 
 router.delete('/:id', (req, res) => {
-    Admin.findByIdAndDelete(req.params.id)
-        .then(admin => res.json('Admin deleted!'))
-        .catch(err => res.status(400).json('Error' + err));
+  Admin.findByIdAndDelete(req.params.id)
+    .then(admin => res.json('Admin deleted!'))
+    .catch(err => res.status(400).json('Error' + err));
 });
 
 router.post('/update/:id', (req, res) => {
-    Admin.findById(req.params.id)
-        .then(admin => {
-            admin.username = req.body.username;
-            admin.email = req.body.email;
-            admin.password = req.body.password;
+  Admin.findById(req.params.id)
+    .then(admin => {
+      admin.username = req.body.username;
+      admin.email = req.body.email;
+      admin.password = req.body.password;
 
-            admin.save()
-                .then(admin => res.json('Admin updated!'))
-                .catch(err => res.status(400).json('Error' + err));
-        })
+      admin.save()
+        .then(admin => res.json('Admin updated!'))
         .catch(err => res.status(400).json('Error' + err));
+    })
+    .catch(err => res.status(400).json('Error' + err));
 });
 
 router.post('/login', function (req, res) {
-  const {username, password} = req.body;
-  
-  Admin.findOne({username: username}, (err, userData) => {
-    if(!err && userData !== null){
-        if(password == userData.password){
-          jwt.sign(
-            { id: userData.id},
-            config.get('jwtSecret'),
-            { expiresIn: 3600},
-            (err, token) => {
-              if(err) throw err;
-              res.json({
-                token,
-                userData
-              })
-            }
-          )
-        }
-        else{
-          res.status(401).send('Incorrect password!');
-        }
+  const { username, password } = req.body;
+
+  Admin.findOne({ username: username }, (err, userData) => {
+    if (!err && userData !== null) {
+      if (password == userData.password) {
+        jwt.sign(
+          { id: userData.id },
+          config.get('jwtSecret'),
+          { expiresIn: 3600 },
+          (err, token) => {
+            if (err) throw err;
+            res.json({
+              token,
+              userData
+            })
+          }
+        )
+      }
+      else {
+        res.status(401).send('Incorrect password!');
+      }
     }
-    else{
+    else {
       res.status(401).send('Incorrect username!');
     }
   })
 })
 
-// Logout
-
 router.all('/logout', (req, res) => {
   const sessUser = req.session.username;
-  if(sessUser !== undefined){
+  if (sessUser !== undefined) {
     req.session.destroy();
     res.status(200).send('Logout successful!');
   }
-  else{
+  else {
     console.log(`${sessUser}`);
     res.status(401).send("You're not logged in!");
   }
--- rentalproject/backend/routes/cars.js
+++ rentalproject/backend/routes/cars.js
@@ -4,34 +4,34 @@ let Car = require('../models/car.model');
 
 var query = { reserved: "NEM" };
 
-router.get('/', (req, res) =>{
+router.get('/', (req, res) => {
     Car.find()
         .then(cars => res.json(cars))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.get('/filtered', (req, res) =>{
+router.get('/filtered', (req, res) => {
     Car.find(query)
         .then(cars => res.json(cars))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
 router.post('/add', (req, res) => {
-        const brand = req.body.brand;
-        const model = req.body.model;
-        const consumption = Number(req.body.consumption);
-        const plateNumber = req.body.plateNumber;
-        const reserved = req.body.reserved;
-    
-        const newCar = new Car({
-            brand,
-            model,
-            consumption,
-            plateNumber,
-            reserved
-        });
-    
-        newCar.save()
+    const brand = req.body.brand;
+    const model = req.body.model;
+    const consumption = Number(req.body.consumption);
+    const plateNumber = req.body.plateNumber;
+    const reserved = req.body.reserved;
+
+    const newCar = new Car({
+        brand,
+        model,
+        consumption,
+        plateNumber,
+        reserved
+    });
+
+    newCar.save()
         .then(() => res.json('Car added!'))
         .catch(err => res.status(400).json('Error ' + err));
 });
@@ -40,7 +40,7 @@ router.get('/:id', (req, res) => {
     Car.findById(req.params.id)
         .then(car => res.json(car))
         .catch(err => res.status(400).json('Error' + err));
-        
+
 });
 
 router.delete('/:id', (req, res) => {
